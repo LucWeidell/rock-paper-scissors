21aef61b99719ec8e5a6661ba549f5bce3ad5d56
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-06-30 13:58:34 -0600

    contained the player hand and generate comp hand

--- app.js
+++ app.js
@@ -14,6 +14,36 @@ let handChoice =(
   btnClass: 'btn-bg-success'
 })
 
+/**
+ * This function will the hand and run through calling the functions to change the state of the site.
+ * This function does not return or output though
+ *
+ * @param {*} userChoice
+ * @throws {Error} if type is wrong or not in dict.
+ */
 function play(userChoice){
+  //Throw error
+  if(!(typeOf(userChoice) == String) || handChoice.includes(userChoice.toLowerCase())){
+    Error("UserChoice choice: not string or not in dict");
+  }
+  p1 = handChoice[userChoice.toLowerCase()]
+  comp = makeCompHand();
+  //So i have both hand names
+
+}
+
+/**
+ * Makes a random hand for the computer: the handChoice dict size does not matter
+ *
+ * @returns {Object} the hand object from handChoice dictionary
+ */
+function compHand(){
+  let allHands = Objects.keys(handChoice);
+  let compHand = Math.round(Math.random()*(allHands.length - 1))
+  return allHands[compHand]
+}
+
+
+function fight(human, computer){
 
 }
