2714f5c934aa80724b7f0662b24c498a7e24651a
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-06-30 13:38:48 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+window.ListeningStateChangedEvent();
+
+let handChoice =(
+{
+  hand: 'rock',
+  btnClass: 'btn-bg-secondary'
+},
+{
+  hand: 'paper',
+  btnClass: 'btn-bg-light'
+},
+{
+  hand: 'scissors',
+  btnClass: 'btn-bg-success'
+})
+
+function play(userChoice){
+
+}
